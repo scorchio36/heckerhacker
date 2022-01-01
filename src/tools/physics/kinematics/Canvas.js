00e9d47f0d74dec2610c1ea4161dd53514ab25c7
scorchio36 <christianhecker36@yahoo.com>
2022-01-01 12:25:12 -0600

    cleaning up projectilesim code a bit:

--- src/tools/physics/kinematics/Canvas.js
+++ src/tools/physics/kinematics/Canvas.js
@@ -3,7 +3,8 @@
 import React, {Component} from 'react';
 
 const MASS = 25;
-const G = 9.81;
+const G = 9.81; // gravitational constant on Earth
+const TIME_STEP = 0.01; // how much time should move forward on each setInterval call
 
 class Canvas extends Component {
 
@@ -11,18 +12,18 @@ class Canvas extends Component {
     super(props);
 
     this.state = {
-      intervalId: 0,
-      x: 0,
-      y: 640,
-      t: 0,
-      x_vel: 0,
-      y_vel: 0,
-      init_x_vel: 0,
-      init_y_vel: 0,
-      total_vel: 0,
-      max_y_height: 0,
-      max_y_height_x_pos: 0,
-      kinetic_energy: 0
+      intervalId: 0, // required to start and stop setInterval function executions
+      x: 0, // current horizontal position of the projectile
+      y: 640, // current vertical position of the the projectile
+      t: 0, // this time variable parametrizes the position values. It is fed into the equations as an independent variable to determine the x and y positions of the projectile.
+      x_vel: 0, // current horizontal velocity component
+      y_vel: 0, // current vertical velocity component
+      init_x_vel: 0, // initial horizontal starting velocity of the projectile from its launch point
+      init_y_vel: 0, // initial vertical starting velocity of the projectile from its launch point
+      total_vel: 0, // the vector sum of the horizontal and vertical velocity components of the projectile
+      max_y_height: 0, // the vertical position of the highest point reached by the projectile
+      max_y_height_x_pos: 0, // the horizontal position of the highest point reached by the projectile
+      kinetic_energy: 0 // current kinetic energy of the projectile
     };
 
     this.handleGoButtonClick = this.handleGoButtonClick.bind(this);
@@ -31,17 +32,19 @@ class Canvas extends Component {
     this.paintGrid = this.paintGrid.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.ex = 0;
-
   }
 
   render() {
 
+      /* use the ref attribute to grab a reference of the rendered canvas. This will be
+       used to get the canvas context. */
       return (
         <div>
+
           <canvas width="960" height="640" id="myCanvas" ref={
           (el) => this._canvas = el
           }/>
+
           <p>X-position: {this.state.x}</p>
           <p>Y-position: {640 - this.state.y}</p>
           <p>V<sub>x</sub>: {this.state.x_vel}</p>
@@ -64,8 +67,14 @@ class Canvas extends Component {
   }
 
   componentDidMount() {
+
+    // grab the canvas context so we can start drawing on the canvas
     this.ctx = this._canvas.getContext('2d');
 
+    /* The vertical of the canvas reference frame is flipped with respect to the typical
+    Euclidian vertical that we deal with in physics. I would like the projectile to be placed
+    at what is considered the Euclidian origin. The canvas height, however, is not available until
+    the component has mounted. So that's why I update the state here.*/
     this.setState(prevState => {
       return {
         ...prevState,
@@ -74,23 +83,31 @@ class Canvas extends Component {
       };
     });
 
+    // paint a grid onto the canvas and label with values
     this.paintGrid();
   }
 
+  // runs when the 'simulate' button is pressed
   handleGoButtonClick() {
 
+    // we need to store this IntervalID to give the clearInterval function
     const IntervalID = setInterval(() => {
 
+      // used to calculate velocity of each respective component.
+      // Stores the current x and y right before they get updated in the first
+      // setState. Those updated values are then compared to these saved ones and
+      // divided by the timestep so we get velocity.
       let prevXPos = this.state.x;
       let prevYPos = this.state.y;
 
-      //update state
+      // if you use the prevState.t here, you can use the previous time value to calucalate the current x an y, but
+      // you can also update the current time value in the same setState call.
       this.setState(prevState => {
         return {
           ...prevState,
-          t: prevState.t + 0.01,
-          y: (0.5*G*(prevState.t*prevState.t) - ((this.state.init_y_vel)*prevState.t) + this._canvas.height),
-          x: prevState.t*(this.state.init_x_vel)
+          t: prevState.t + TIME_STEP,
+          y: (0.5*G*(prevState.t*prevState.t) - ((this.state.init_y_vel)*prevState.t) + this._canvas.height), // y(t) = -(1/2)(g)(t^2) + (V_oy)(t) + (y_init)
+          x: prevState.t*(this.state.init_x_vel) // x(t) = (V_ox)(t) + (x_init)
         };
       });
 
@@ -99,8 +116,8 @@ class Canvas extends Component {
         return {
           ...prevState,
           intervalID: IntervalID,
-          x_vel: (this.state.x - prevXPos)/0.01,
-          y_vel: (-0.1)*(this.state.y - prevYPos)/0.01,
+          x_vel: (this.state.x - prevXPos)/TIME_STEP,
+          y_vel: (-0.1)*(this.state.y - prevYPos)/TIME_STEP,
           total_vel: (Math.sqrt((this.state.x_vel*this.state.x_vel)+(this.state.y_vel*this.state.y_vel))),
           max_y_height: Math.max((this._canvas.height-this.state.y), (prevState.max_y_height)),
           kinetic_energy: (0.5*MASS*this.state.total_vel*this.state.total_vel)
@@ -116,7 +133,6 @@ class Canvas extends Component {
           }
         });
 
-        console.log(this.state.max_y_height_x_pos);
       }
 
       this.ctx.beginPath();
@@ -129,8 +145,6 @@ class Canvas extends Component {
       if (this._canvas.height-this.state.y < 0.001 && (this._canvas.height-this.state.y != 0)) {
 
         clearInterval(this.state.intervalID);
-
-        console.log(this.state.max_y_height);
 
         // write out the max height on the screen
         this.ctx.font = '12px serif';
